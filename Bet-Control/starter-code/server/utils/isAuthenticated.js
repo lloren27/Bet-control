0977127357b248b0ee822d4e7a719849d932909f
Carlos Llorente <llorenteyuste@hotmail.com>
2018-05-16 23:55:37 +0200

    fuera

--- Bet-Control/starter-code/server/utils/isAuthenticated.js
+++
@@ -1,8 +0,0 @@
-function isAuthenticated(req, res, next){
-  if (!req.isAuthenticated()){
-     return res.status(403).json(new Error("Not Authorized"))
-  }
-  return next();
-}
-
-module.exports = isAuthenticated;
